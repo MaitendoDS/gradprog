552ddd0bea4fd6285138c965d644f990056f70c4
MaitendoDS <147160888+MaitendoDS@users.noreply.github.com>
2023-11-01 16:21:15 +0100

    add

---
+++ prog1/maitedesmet/D07/D07begintmethoofdletter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D07
+{
+    internal class D07begintmethoofdletter
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef een stukje tekst:");
+            string input = Console.ReadLine();
+
+            char c = input[0];
+            bool hoofdletter = char.IsUpper(c);
+
+            if (hoofdletter) { Console.WriteLine("De tekst start met een hoofdletter."); }
+            else { Console.WriteLine("De tekt start met een kleine letter."); }
+        }
+    }
+}
---
+++ prog1/maitedesmet/D07/aantalkeere.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D07
+{
+    internal class aantalkeere
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef een tekst:");
+            string input = Console.ReadLine();
+
+            int aantalKeer = 0;
+
+            foreach (char c in input) {
+                if (c == 'e'| c=='E') {
+                    aantalKeer++;
+                }
+            }
+            Console.WriteLine($"De letter 'e' komt {aantalKeer} voor.");
+
+        }
+    }
+}
