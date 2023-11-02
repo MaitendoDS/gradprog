530311720968cf2ee4d20db029cb28c88c9cff52
MaitendoDS <147160888+MaitendoDS@users.noreply.github.com>
2023-11-02 19:35:51 +0100

    aaa

---
+++ prog1/maitedesmet/D07/D07beginmethoofdletters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace D07
+{
+    internal class D07beginmethoofdletters
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("geef een tekst:");
+            string tekst = Console.ReadLine();
+            
+
+            for (int i = 0; i <= tekst.Length-1; i++){
+                
+            char c = tekst[i];
+                if (i<5) {
+                    c = char.ToUpper(c);
+                }
+        
+
+        }
+    }
+}
