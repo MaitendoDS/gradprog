b8eca9cfe53eac5aeb5f2cc3aeb303215b8a45f0
MaitendoDS <147160888+MaitendoDS@users.noreply.github.com>
2023-11-16 13:44:12 +0100

    sjdfhlskj

---
+++ prog1/maitedesmet/D10/D10toonvierkant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D10
+{
+    internal class D10toonvierkant
+    {
+        static void Main()
+        {
+            ToonVierkant();
+        }
+        static void ToonVierkant()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("*****");
+            }
+            // Console.WriteLine();
+        }
+    }
+}
---
+++ prog1/maitedesmet/D10/D10toonvierkantparameters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D10
+{
+    internal class D10toonvierkantparameters
+    {
+        static void Main(string[] args)
+        {
+            ToonVierkantParameters();
+        }
+
+
+        static void ToonVierkantParameters()
+        {
+            Console.Write("Zijde?: ");
+            int zijde = int.Parse(Console.ReadLine());
+
+
+            int hoogteTeller = 0;
+
+
+            do
+            {
+                int breedteTeller = 0;
+                do
+                {
+                    Console.Write("*");
+                    breedteTeller++;
+
+
+                } while (breedteTeller < zijde);
+
+                Console.WriteLine();
+                hoogteTeller++;
+
+            } while (hoogteTeller < zijde);
+        }
+    }
+}
