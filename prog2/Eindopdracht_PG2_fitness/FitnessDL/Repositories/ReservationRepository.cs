6f54fb3f6f91b588f8e9e112ef5f34cf366e2dc9
MaitendoDS <maitedesmet77@gmail.com>
2024-12-16 00:23:19 +0100

    reservations af

--- prog2/Eindopdracht_PG2_fitness/FitnessDL/Repositories/ReservationRepository.cs
+++ prog2/Eindopdracht_PG2_fitness/FitnessDL/Repositories/ReservationRepository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace FitnessDL.Repositories
 {
-    public class ReservationRepository: IReservationRepository
+    public class ReservationRepository : IReservationRepository
     {
 
         FitnessContext _context;
@@ -31,10 +31,75 @@ namespace FitnessDL.Repositories
                 }
 
                 var reservationEF = MapReservation.MapToDL(reservation);
+
+                if (reservation.Date >= DateTime.Now.AddDays(7))
+                {
+                    throw new Exception("Mag niet meer dan 7 dagen in de toekomst zijn");
+                }
+
+
+                var equipment = _context.Equipment.FirstOrDefault(e => e.EquipmentID == reservation.EquipmentID);
+                if (equipment == null)
+                {
+                    throw new Exception("Equipment is null");
+                }
+
+                var timeslot = _context.Time_Slot.FirstOrDefault(e => e.TimeSlotID == reservation.TimeSlotID);
+                if (timeslot == null)
+                {
+                    throw new Exception("TimeSlot is null");
+                }
+
+                List<ReservationEF> reservations = _context.Reservations.Where(r => r.MemberID == reservation.MemberID && r.Date.Date == reservation.Date.Date).ToList();
+
+                if (reservations.Count >= 4)
+                {
+                    throw new Exception("Meer dan 4 reservaties al deze dag");
+                }
+
+                List<ReservationEF> equipmentlist = _context.Reservations.Where(e => e.EquipmentID == reservation.EquipmentID && e.Date.Date == reservation.Date.Date).ToList();
+
+                if (equipmentlist.Any(e => e.TimeSlotID == reservation.TimeSlotID))
+                {
+                    throw new Exception("Toestel al in gebruik");
+                }
+
+               
+                List<ReservationEF> equipmentListMember = _context.Reservations.Where(e => e.EquipmentID == reservation.EquipmentID && e.Date.Date == reservation.Date.Date && e.MemberID == reservation.MemberID).ToList();
+                List<Time_SlotEF> timeSlots = equipmentListMember.Select(e => _context.Time_Slot.FirstOrDefault(t => t.TimeSlotID == e.TimeSlotID)).ToList();
+
+                timeSlots.Add(timeslot);
+                timeSlots = timeSlots.OrderBy(t => t.StartTime).ToList();
+
+                for(int i = 0;i < timeSlots.Count -2;i++)
+                {
+                    if (timeSlots[i+1].TimeSlotID - timeSlots[i].TimeSlotID == 1 && timeSlots[i+2].TimeSlotID - timeSlots[i+1].TimeSlotID == 1)
+                    {
+                        throw new Exception("Mag maar 2 slots na elkaar met zelfde toestel");
+                    }
+                    
+                }
+
                 _context.Reservations.Add(reservationEF);
                 _context.SaveChanges();
 
                 return MapReservation.MapToBL(reservationEF);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             }
             catch (Exception x)
@@ -62,7 +127,7 @@ namespace FitnessDL.Repositories
                 }
                 else
                 {
-                    throw new Exception("Reservation niet gevonden");
+                    throw new Exception("Reservation niet gevonden"); 
                 }
 
 
